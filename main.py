951bd466cbe53c23e0c3d01b28bde4b76059f3b8
EslamSallam <eslam.salam1369@gmail.com>
2022-04-03 11:35:36 +0200

    adding new files

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, status
+from typing import Optional
+
+from Item import Item
 
 app = FastAPI()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
-
+# simple Get root Function
 @app.get("/")
 def home():
-    return {"message":"Hello TutLinks.com"}
+    return {"Data": "Test"}
+
+
+# simple Get About function
+@app.get("/about")
+def about():
+    return {"Data": "About"}
+
+
+inventory = {
+    1: {
+        "name": "Milk",
+        "price": 3.99,
+        "brand": "Regular"
+    }
+}
+
+
+# Path Params Example
+@app.get("/get-item/{item_id}/{name}")
+def get_item(item_id: int = Path(None, description="The Id of the item you would like to view", gt=0, lt=2),
+             name: str = None):
+    return inventory[item_id]
+
+
+# Query params Example
+# passing test case: http://127.0.0.1:8000/get-by-name/1?name=Milk&test=1
+@app.get("/get-by-name/{item_id}")
+def get_item(*, item_id: int, name: Optional[str] = None, test: int):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item name not found")
+
+
+# simple post request implementation
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    for id in inventory:
+        if item_id == id:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Item id already Added")
+    inventory[item_id] = item
+    return inventory[item_id]
