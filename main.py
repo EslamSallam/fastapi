af10cb6ac65094ced82862562101665f0c3572c5
EslamSallam <eslam.salam1369@gmail.com>
2022-04-06 10:51:53 +0200

    Revert "Testing V1"
    This reverts commit 34c8bef792d5c03285ccd938ab13610bfc1057f1.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from Item import Item
 
 app = FastAPI()
 
+
+
 # simple Get About function
 @app.get("/about")
 def about():
@@ -52,5 +54,5 @@ def create_item(item_id: int, item: Item):
 def OCR_EG_NationalID_Front(SecurityKey: str, ImageUrl: str):
     if SecurityKey == APIVault.GetSecurityKey():
         im = Image.open(requests.get(ImageUrl, stream=True).raw)
-        return "Success"
+        return im
     return "Failed"
